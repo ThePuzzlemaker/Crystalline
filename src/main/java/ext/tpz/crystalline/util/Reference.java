26609e4410ed670c19b9a24896b4aa2d38fe043b
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2018-12-02 11:38:54 -0600

    Finish guide (closes #7)

--- src/main/java/ext/tpz/crystalline/util/Reference.java
+++ src/main/java/ext/tpz/crystalline/util/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
     public static final String MODID = "crystalline";
     public static final String MODVER = "1.0.0";
     public static final String MODNAME = "Crystalline";
-    public static final String MODDEPS = "required-after:forge@[11.16.0.1865,);before:guideapi@[1.12-2.1.5-60,)";
+    public static final String MODDEPS = "required-after:forge@[11.16.0.1865,);before:guideapi@[1.12-2.1.5-60,);after:patchouli@[1.0-10,)";
 
     public static final String PROXYSERVER = "ext.tpz.crystalline.proxy.ServerProxy";
     public static final String PROXYCLIENT = "ext.tpz.crystalline.proxy.ClientProxy";
