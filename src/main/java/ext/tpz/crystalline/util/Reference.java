db48b9dd586b2c926211963bba3b8b865f9514e8
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2018-12-03 19:04:32 -0600

    Change specific version to ambiguous "1.12.2-git"

--- src/main/java/ext/tpz/crystalline/util/Reference.java
+++ src/main/java/ext/tpz/crystalline/util/Reference.java
@@ -5,7 +5,7 @@ import ext.tpz.crystalline.item.CrystallineItems;
 public class Reference {
 
     public static final String MODID = "crystalline";
-    public static final String MODVER = "1.0.0";
+    public static final String MODVER = "1.12.2-git";
     public static final String MODNAME = "Crystalline";
     public static final String MODDEPS = "required-after:forge@[11.16.0.1865,);before:guideapi@[1.12-2.1.5-60,);after:patchouli@[1.0-10,)";
 
