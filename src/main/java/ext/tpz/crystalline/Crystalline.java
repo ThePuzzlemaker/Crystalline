57223d910ced66bfda6e65c465b0968c5c17b3e3
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2019-01-19 21:55:36 -0600

    Crystalline command

--- src/main/java/ext/tpz/crystalline/Crystalline.java
+++ src/main/java/ext/tpz/crystalline/Crystalline.java
@@ -1,6 +1,7 @@
 package ext.tpz.crystalline;
 
 import ext.tpz.crystalline.proxy.CommonProxy;
+import ext.tpz.crystalline.util.CommandCrystalline;
 import ext.tpz.crystalline.util.Reference;
 import ext.tpz.crystalline.util.TabCrystalline;
 import net.minecraft.creativetab.CreativeTabs;
@@ -10,6 +11,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.server.permission.DefaultPermissionLevel;
+import net.minecraftforge.server.permission.PermissionAPI;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MODID, name = Reference.MODNAME, version = Reference.MODVER, dependencies = Reference.MODDEPS, useMetadata = true)
@@ -39,6 +42,12 @@ public class Crystalline {
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent e) {
         proxy.postInit(e);
+    }
+
+    @Mod.EventHandler
+    public void serverLoad(FMLServerStartingEvent e) {
+        PermissionAPI.registerNode("crystalline.crystalline", DefaultPermissionLevel.OP, "A debug command for setting insanity or listing all crystal types.");
+        e.registerServerCommand(new CommandCrystalline());
     }
 
 }
