ca7ccb1f4aa444d76010715388ac7c73a41be1d4
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2019-06-22 19:11:15 -0500

    Streamline GUI code and remove unneccessary indicator image file

--- src/main/java/ext/tpz/crystalline/common/block/tuning/TuningMachineGUI.java
+++ src/main/java/ext/tpz/crystalline/common/block/tuning/TuningMachineGUI.java
@@ -24,9 +24,9 @@ public class TuningMachineGUI extends GuiContainer {
 
     private int difference = 0;
     private int indicY = 0;
+    private int finalIndicY = 0;
 
     private static final ResourceLocation background = new ResourceLocation(CStatic.MODID, "textures/gui/tuningmachine.png");
-    private static final ResourceLocation indicator = new ResourceLocation(CStatic.MODID, "textures/gui/tuningmachine-indicator.png");
 
     private TETuningMachine te;
 
@@ -57,9 +57,20 @@ public class TuningMachineGUI extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY) {
-        mc.getTextureManager().bindTexture(background);
-        drawTexturedModalRect(guiLeft, guiTop, 0, 0, xSize, ySize);
+        // Set draw-scope constants and render main background
+        renderTexturedModalRect(background, guiLeft, guiTop, 0, 0, xSize, ySize);
+        setDrawConstants(guiLeft, guiTop);
 
+        // Draw labels
+        drawTextCentered(tuneX + tuneW / 2, tuneY + tuneH / 2 - 4, "TUNE");
+        drawTextCentered(testX + testW / 2, testY + testH / 2 - 4, "TEST");
+
+        // Iterate indicator's position and draw the indicator
+        iterateIndicatorPos(guiLeft, guiTop);
+        drawHorizontalLine(guiLeft + 144, guiLeft + 144 + 7, indicY, 0x000000);
+    }
+
+    private void setDrawConstants(int guiLeft, int guiTop) {
         tuneX = guiLeft + 50;
         tuneY = guiTop + 31;
         tuneW = 38;
@@ -70,8 +81,18 @@ public class TuningMachineGUI extends GuiContainer {
         testW = 38;
         testH = 14;
 
-        int finalIndicY = guiTop + 35 + MathHelper.clamp(te.getCurrentDifference(), -30, 30);
+        finalIndicY = guiTop + 35 + MathHelper.clamp(te.getCurrentDifference(), -30, 30);
+    }
 
+    private void drawText(int x, int y, String text) {
+        mc.fontRenderer.drawString(text, x, y, 0x000000);
+    }
+
+    private void drawTextCentered(int x, int y, String text) {
+        drawText(x - (mc.fontRenderer.getStringWidth(text) / 2), y, text);
+    }
+
+    private void iterateIndicatorPos(int guiLeft, int guiTop) {
         if (indicY < guiTop + 5 || indicY > guiTop + 565)
             indicY = guiTop + 35;
 
@@ -80,12 +101,13 @@ public class TuningMachineGUI extends GuiContainer {
         } else if (indicY > finalIndicY) {
             indicY--;
         }
-
-        mc.getTextureManager().bindTexture(indicator);
-        drawTexturedModalRect(guiLeft + 144, indicY, 0, 0, 7, 1);
-
-        new Label.LabelCentered(tuneX + tuneW / 2, tuneY + tuneH / 2 - 4, "TUNE", 0x000000, false).draw(mouseX, mouseY);
-        new Label.LabelCentered(testX + testW / 2, testY + testH / 2 - 4, "TEST", 0x000000, false).draw(mouseX, mouseY);
     }
+
+    private void renderTexturedModalRect(ResourceLocation tex, int x, int y, int texx, int texy, int sizex, int sizey) {
+        mc.getTextureManager().bindTexture(tex);
+        drawTexturedModalRect(x, y, texx, texy, sizex, sizey);
+    }
+
+
 
 }
