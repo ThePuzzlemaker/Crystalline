2dce39efc89df4a6bcef0c8d8989b5d7e45f0674
Dak Smyth <tpzker@thepuzzlemaker.info>
2019-08-03 21:29:23 -0500

    May help if I actually stage all changes

--- src/main/java/com/teamisotope/crystalline/api/CStatic.java
+++ src/main/java/com/teamisotope/crystalline/api/CStatic.java
@@ -5,7 +5,7 @@ public class CStatic {
     public static final String MODID = "crystalline";
     public static final String MODVER = "1.12.2-git";
     public static final String MODNAME = "Crystalline";
-    public static final String MODDEPS = "required-after:forge@[14.23.5.2838,)";
+    public static final String MODDEPS = "required-after:forge@[14.23.5.2838,);required-after:patchouli@[1.0-20,)";
 
     public static final String PROXY_SERVER = "com.teamisotope.crystalline.server.ServerProxy";
     public static final String PROXY_CLIENT = "com.teamisotope.crystalline.client.ClientProxy";
