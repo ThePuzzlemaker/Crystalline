37b21ecf5d099ee766d017ed1b6ac58f48017dd3
Dak Smyth <tpzker@thepuzzlemaker.info>
2019-11-20 15:21:27 -0600

    Fix version.sh

--- version.sh
+++ version.sh
@@ -6,6 +6,8 @@ source ./version.info
 if [[ "$VERSION" == *-git ]]; then
 	sed -i "s/-git/-git.`git show -s --format=\"%ct\" master`/g" version.info
 fi
+
+source ./version.info
 	
 sed -i "s/1.12.2-git/$VERSION/g" src/main/java/com/teamisotope/crystalline/api/CStatic.java
 sed -i "s/1.12.2-git/$VERSION/g" build.gradle
