c8849a4c89df025c5c05c84360289a4c14bbe775
Dak Smyth <tpzker@thepuzzlemaker.info>
2019-11-20 15:00:26 -0600

    Setup for ModMaven deployment

--- version.sh
+++ version.sh
@@ -3,5 +3,9 @@
 export VERSION="1.12.2-git"
 source ./version.info
 
+if [[ "$VERSION" == *-git ]]; then
+	sed -i "s/-git/-git.`git show -s --format=\"%ct\" master`/g" version.info
+fi
+	
 sed -i "s/1.12.2-git/$VERSION/g" src/main/java/com/teamisotope/crystalline/api/CStatic.java
 sed -i "s/1.12.2-git/$VERSION/g" build.gradle
