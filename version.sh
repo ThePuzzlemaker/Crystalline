003f5332566a67ef3550672c4773be23b3ad0a30
ThePuzzlemaker <tpzker@thepuzzlemaker.info>
2020-02-25 16:48:02 -0600

    Re-re-fix auto-versioning script (should probably first test my scripts)

--- version.sh
+++ version.sh
@@ -3,9 +3,9 @@
 export VERSION="1.0.0+experimental.git.1.15.2"
 source ./version.info
 
-if [[ "$VERSION" == *.git. ]]; then
-	export GITTIME=$(git show -s --format=\"%ct\" HEAD)
-	sed -i "s/.git./.git.${GITTIME}./g" version.info
+if [[ "$VERSION" == *.git.* ]]; then
+	export GITTIME=$(git show -s --format=%ct HEAD)
+	sed -i "s/.git./.git.$GITTIME./g" version.info
 fi
 
 source ./version.info
